1f9e96972818e6a6d8c10e6f516a59cc696d2bf5
SalvadorRdR <salvador.romero.d@uni.pe>
2024-12-15 02:07:10 -0500

    cambio_sabado

--- Transmision/Prueba_Transmision.ino
+++ Transmision/Prueba_Transmision.ino
@@ -1,45 +1,51 @@
-#include <SPI.h>
+#include <SPI.h> // Incluye la biblioteca SPI para la comunicación SPI
 
-// Comentario de prueba
-// Data Pins
-#define RECIEVE_PIN 16
-#define TRANSMIT_PIN 17
-#define CLK_OUT_PIN 21
-// Setup Pins
-#define CE_PIN 5
-#define SREAD_PIN 19
-#define SDATA_PIN 23
-#define SCLK_PIN 18
-#define SLE_PIN 32
-// Flatsat tx/rx
-#define FS_TX_PIN 1
-#define FS_RX_PIN 3
 
-const uint32_t REG0 = 0x11616A20;
-const uint32_t REG1 = 0x00475011;
-const uint32_t REG2 = 0x003DD082;  
-const uint32_t REG3 = 0x2B149923; 
-const uint32_t REG15 = 0x000E000F;
+// Definición de pines para la comunicacion de datos
+#define RECIEVE_PIN 16 // Pin para recibir datos
+#define TRANSMIT_PIN 17 // Pin para transmitir datos
+#define CLK_OUT_PIN 21 // Pin para el reloj de salida
 
-#define MAX_PACKET_SIZE 256 // Define a max size for telemetry packets
-#define BUFFER_SIZE 10      // Define the number of packets to store
+// Definición de pines para la configuración del ADF7021
+#define CE_PIN 5 // Pin para la habilitación del chip
+#define SREAD_PIN 19 // Pin para la lectura de datos en serie
+#define SDATA_PIN 23 // Pin para la escritura de datos en serie
+#define SCLK_PIN 18 // Pin para el reloj de entrada
+#define SLE_PIN 32 // Pin para la carga de datos en registros
 
-uint8_t telemetryBuffer[BUFFER_SIZE][MAX_PACKET_SIZE]; // Buffer to store telemetry packets
-uint8_t packetLengths[BUFFER_SIZE]; // Array to store the lengths of the packets
-uint8_t bufferIndex = 0; // Index to keep track of the current packet
+// Definición de pines para la comunicación con el FlatSat
+#define FS_TX_PIN 1 // Pin para la transmisión de datos en el FlatSat
+#define FS_RX_PIN 3 // Pin para la recepción de datos en el FlatSat
 
+// Definición de los valores de los registros del ADF7021
+const uint32_t REG0 = 0x11616A20; // Valor del registro 0
+const uint32_t REG1 = 0x00475011; // Valor del registro 1
+const uint32_t REG2 = 0x003DD082; // Valor del registro 2
+const uint32_t REG3 = 0x2B149923; // Valor del registro 3
+const uint32_t REG15 = 0x000E000F; // Valor del registro 15
+
+// Definición de constantes para el tamaño de los paquetes de telemetría y el buffer
+#define MAX_PACKET_SIZE 256 // Define el tamaño máximo de los paquetes de telemetría
+#define BUFFER_SIZE 10 // Define el número de paquetes que se pueden almacenar
+
+// Definición de buffers para almacenar los paquetes de telemetría
+uint8_t telemetryBuffer[BUFFER_SIZE][MAX_PACKET_SIZE]; // Buffer para almacenar los paquetes de telemetría
+uint8_t packetLengths[BUFFER_SIZE]; // Array para almacenar las longitudes de los paquetes
+uint8_t bufferIndex = 0; // Índice para llevar un seguimiento del paquete actual
+
+// Función para configurar los pines
 void setupPins(){
-  pinMode(SLE_PIN, OUTPUT);
-  pinMode(TRANSMIT_PIN, OUTPUT);
-  pinMode(RECIEVE_PIN, INPUT);
-  pinMode(CLK_OUT_PIN, INPUT);
-  pinMode(SCLK_PIN, OUTPUT);
+  pinMode(SLE_PIN, OUTPUT); // Configura el pin SLE como salida
+  pinMode(TRANSMIT_PIN, OUTPUT); // Configura el pin TRANSMIT como salida
+  pinMode(RECIEVE_PIN, INPUT); // Configura el pin RECIEVE como entrada
+  pinMode(CLK_OUT_PIN, INPUT); // Configura el pin CLK_OUT como entrada
+  pinMode(SCLK_PIN, OUTPUT); // Configura el pin SCLK como salida
 
-  pinMode(FS_RX_PIN, INPUT);
-  pinMode(FS_TX_PIN, OUTPUT);
+  pinMode(FS_RX_PIN, INPUT); // Configura el pin FS_RX como entrada
+  pinMode(FS_TX_PIN, OUTPUT); // Configura el pin FS_TX como salida
 
-  digitalWrite(SLE_PIN, LOW); 
-  digitalWrite(TRANSMIT_PIN, LOW); 
+  digitalWrite(SLE_PIN, LOW); // Establece el pin SLE en bajo
+  digitalWrite(TRANSMIT_PIN, LOW); // Establece el pin TRANSMIT en bajo
 }
 
 void writeRegister(uint32_t data, uint8_t length) {
